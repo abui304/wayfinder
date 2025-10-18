58cd7f1ac13c79d8800aa465021f8739c1e848d1
abui304 <atb2923@utexas.edu>
2025-10-18 17:59:35 -0500

    react set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1>This is the app, add routing and other stuff here</h1>
+            <div>This is the highest layer, do not add anything detaily here</div>
+        </div>
+    )
+}
